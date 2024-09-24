f3efd740d35f168da88570f938f8d1f977e21891
Artyom <agrhxd@yandex.ru>
2024-09-24 18:59:17 +0300

    client dict tests

--- tests/test_client/conftest.py
+++ tests/test_client/conftest.py
@@ -1,23 +1,13 @@
 import pytest
-import socket_chat.connection as connection
-import socket_chat.server as server
-from socket_chat.tsdict import ThreadSafeDict
-import threading
 import socket
-
-
-@pytest.fixture(scope='session')
-def clients_dict():
-    clients_dict = ThreadSafeDict()
-    return clients_dict
+from socket_chat.tsdict import ThreadSafeDict
+from socket_chat.server import ClientHandler
+from socket_chat.connection import Connection
+import threading
 
 
 @pytest.fixture(scope='session', autouse=True)
-def test_server(clients_dict):
-    threading.Thread(target=start_server, args=(clients_dict, 54321,), daemon=True).start()
-
-
-def start_server(clients_dict, host_port, host_ip = socket.gethostbyname(socket.gethostname())):
+def start_server(host_port, host_ip = socket.gethostbyname(socket.gethostname())):
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
         server_socket.bind((host_ip, host_port))
@@ -26,13 +16,14 @@ def start_server(clients_dict, host_port, host_ip = socket.gethostbyname(socket.
     except Exception as e:
         print(f'[-]Error while starting the server: {e}')
         server_socket.close()
+
+    clients = ThreadSafeDict()
     while True:
         try:
             client_socket, client_addr = server_socket.accept()
             print(f"[*]Accepted connection from {client_addr[0]}:{client_addr[1]}")
-            handler = server.ClientHandler(connection.Connection(client_socket), clients_dict)
+            handler = ClientHandler(Connection(client_socket), clients)
             threading.Thread(target=handler.run, daemon=True).start()
         except Exception as e:
             print(f'[-]Error while accepting the client: {e}')
             client_socket.close()
-
--- tests/test_server/test_threadsafedict.py
+++ tests/test_server/test_threadsafedict.py
@@ -9,9 +9,11 @@ import threading
 import time 
 
 
-username_list = ['Artyom', 'Baho', 'Poteha', 'Vlados', 'Diman']
+usernames_list = ['Artyom', 'Baho', 'Poteha', 'Vlados', 'Diman']
 creation_queue = Queue()
 leave_queue = Queue()
+clients = ThreadSafeDict()
+
 
 def start_client(username):
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -30,23 +32,49 @@ def start_client(username):
     
 
 def test_client_usernames(start_server):
+    threading.Thread(target=monitor_dict, daemon=True).start()
     for i in range(5):
-        threading.Thread(target=start_client, args=(username_list[i],),daemon=True).start()
+        threading.Thread(target=start_client, args=(usernames_list[i],),daemon=True).start()
     
-    clients = ThreadSafeDict()
     client_sockets = []
-    for i in range(5):
+    for i in range(len(usernames_list)):
         try:
             client_socket, client_addr = start_server.accept()
             client_sockets.append(client_socket)
             print(f"[*]Accepted connection from {client_addr[0]}:{client_addr[1]}")
             handler = server.ClientHandler(connection.Connection(client_socket), clients)
             threading.Thread(target=handler.run, daemon=True).start()
+            assert usernames_list[i] == creation_queue.get()
         except Exception as e:
             print(f'[-]Error while accepting the client: {e}')
             client_socket.close()
-    time.sleep(1)
-    assert len(clients) == 5
-    client_sockets[1].close()
-    time.sleep(1)
-    assert len(clients) == 4
+
+    assert clients.copy_keys() == usernames_list
+    
+    for i in range(len(usernames_list)):
+        client_sockets[i].close()
+        closed_user = leave_queue.get()
+        assert closed_user not in clients.copy_keys()
+        assert closed_user in usernames_list
+
+    assert clients == {}
+
+
+def monitor_dict():
+    previous_keys = set(clients.copy_keys())
+
+    while True:
+        current_keys = set(clients.copy_keys())
+        
+        new_values = current_keys - previous_keys
+        if new_values:
+            for key in new_values:
+                creation_queue.put(key)
+        
+        removed_values = previous_keys - current_keys
+        if removed_values:
+            for key in removed_values:
+                leave_queue.put(key)
+        
+        previous_keys = current_keys
+        
