6de1d9ccf9f5b6956609a693bcbaf610f8bda44b
Artyom <agrhxd@yandex.ru>
2024-11-06 12:35:15 +0300

    new asyncio realisation

--- source/socket_chat/connection.py
+++ source/socket_chat/connection.py
@@ -1,33 +1,37 @@
-import threading
 import socket
+import asyncio
 
-
+class ConnectionTimedOut():
+    pass
 class Connection:
 
-    def __init__(self, sock: socket.socket):
-        self.sock = sock
-        sock.ioctl(socket.SIO_KEEPALIVE_VALS, (1, 20000, 5000))
+    def __init__(self, client_reader: asyncio.StreamReader, client_writer: asyncio.StreamWriter):
+        self.reader = client_reader
+        self.writer = client_writer
         self.is_active = True
-
-        self.sendLock = threading.Lock()
-
-
-    def send(self, msg: bytes):
-        with self.sendLock:
-            self.sock.sendall(msg)
+        self.sendLock = asyncio.Lock()
 
 
-    def recv(self, len: int) -> bytes:
-        msg = self.sock.recv(len)
-        if not msg:
-            raise socket.error
-        return msg
+    # def get_addr(self):
+    #     return self.writer.get_extra_info('peername')
+
+
+    async def send(self, msg: bytes):
+        self.writer.write(msg)
+        await self.writer.drain()
+
+
+    async def recv(self, len: int, timeout=None) -> bytes:
+        try:
+            msg = await asyncio.wait_for(self.reader.read(len), timeout)
+            if not msg:
+                raise socket.error
+            return msg
+        except TimeoutError:
+            pass
     
 
-    def close(self):
+    async def close(self):
         self.is_active = False
-        self.sock.close()
-
-
-    def settimeout(self, time):
-        self.sock.settimeout(time)
+        self.writer.close()
+        await self.writer.wait_closed()
--- source/socket_chat/server.py
+++ source/socket_chat/server.py
@@ -1,184 +1,162 @@
 import socket
-import threading
-
-from socket_chat.tsdict import ThreadSafeDict
+import asyncio
+import logging.config
 from socket_chat.connection import Connection
 import socket_chat.protocol as protocol
 
 class Server:
     def __init__(self, host_port, host_ip = socket.gethostbyname(socket.gethostname())):
+        self.logger = logging.getLogger(__name__)
         self.host_port = host_port
         self.host_ip = host_ip
         self.addr = (self.host_ip, self.host_port)
-        self.clients = ThreadSafeDict()
+        self.clients: dict = {}
         self.server_socket: socket.socket = None
 
 
-    def start_server(self):
-        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    async def start_server(self):
         try:
-            self.server_socket.bind(self.addr)
-            self.server_socket.listen()
+            self.server = await asyncio.start_server(self.client_handler, self.host_ip, self.host_port)
         except Exception as e:
-            print(f'[-]Error while starting the server: {e}')
-            self.server_socket.close()
+            self.logger.exception(f'[-]Error while starting the server: {e}')
+            self.server.close()
             raise
-        print('[*]Server is listening...\n')
-        self.accept_clients()
+        self.logger.info('[*]Server is listening...\n')
+        await self.server.serve_forever()
 
-
-    def accept_clients(self):
-        while True:
-            try:
-                client_socket, client_addr = self.server_socket.accept()
-                print(f"[*]Accepted connection from {client_addr[0]}:{client_addr[1]}")
-                handler = ClientHandler(Connection(client_socket), self.clients)
-                threading.Thread(target=handler.run, daemon=True).start()
-            except socket.error as e:
-                if e.errno == socket.errno.WSAENOTSOCK:
-                    print(f'[-]Server socket was closed while accepting clients')
-                    raise
-                else:
-                    print(f'[-]Socket error while accepting clients: {e}')
-            except Exception as e:
-                print(f'[-]Exception error while accepting clients: {e}')
-                if client_socket:
-                    client_socket.close()
-                
+    
+    async def client_handler(self, client_reader, client_writer):
+        handler = ClientHandler(Connection(client_reader, client_writer), self.clients)
+        await handler.run()
 
 
 class ClientHandler:
-    def __init__(self, client: Connection, clients: ThreadSafeDict):
+    def __init__(self, client: Connection, clients: dict):
+        self.logger = logging.getLogger(__name__)
         self.client = client
         self.clients = clients
         self.username = ''
+        self.timeout = 5
     
     
-    def run(self):
+    async def run(self):
         try:
             while self.client.is_active:
-                hdr, payload = self.recv_pkt()
+                hdr, payload = await self.recv_pkt()
                 self.handle_pkt(hdr, payload)
         except Exception as e:
-            print(f'[-]Error: {e}')
+            self.logger.exception(f'[-]Error: {e}')
             self.shutdown()
 
 
-    def recv_pkt(self):
+    async def recv_pkt(self):
         hdr_len = protocol.chat_header.PKT_TYPE_FIELD_SIZE + protocol.chat_header.PKT_LEN_FIELD_SIZE
         hdr_bytes = b''
-        hdr_bytes += self.client.recv(hdr_len)
+        hdr_bytes += await self.client.recv(hdr_len)
         
-        self.client.settimeout(5)
         while len(hdr_bytes) < hdr_len:
-            hdr_bytes += self.client.recv(hdr_len - len(hdr_bytes))
+            hdr_bytes += await self.client.recv((hdr_len - len(hdr_bytes)), self.timeout)
         hdr = protocol.chat_header()
         hdr.unpack(hdr_bytes)
 
         payload_len = hdr.msg_len
         payload = b''
         while len(payload) < payload_len:
-            payload += self.client.recv(payload_len - len(payload))
-        self.client.settimeout(None)
+            payload += await self.client.recv((payload_len - len(payload)), self.timeout)
         return hdr, payload
     
 
-    def handle_pkt(self, hdr, payload):
+    async def handle_pkt(self, hdr: protocol.chat_header, payload: bytes):
         if not self.username and hdr.msg_type != protocol.MSG_TYPE.CHAT_CONNECT.value:
             raise ValueError('Invalid packets from unauthorized user')
         match hdr.msg_type:
             case protocol.MSG_TYPE.CHAT_CONNECT.value:
                 pkt = protocol.chat_connect()
                 pkt.unpack(payload)
-                self.handle_connect(pkt)
+                await self.handle_connect(pkt)
             case protocol.MSG_TYPE.CHAT_MSG.value:
                 pkt = protocol.chat_msg()
                 pkt.unpack(payload)
-                self.handle_msg(pkt)
+                await self.handle_msg(pkt)
             case protocol.MSG_TYPE.CHAT_DISCONNECT.value:
-                self.shutdown()
+                await self.shutdown()
             case protocol.MSG_TYPE.CHAT_COMMAND.value:
                 pkt = protocol.chat_command()
                 pkt.unpack(payload)
-                self.handle_command(pkt)
+                await self.handle_command(pkt)
             case _:
-                raise protocol.WrongProtocolTypeError(f"[-]Unexpected type {hdr.msg_type}")
+                raise protocol.WrongProtocolTypeError(f"[-]Unexpected type {hdr.msg_type} in handle_pkt")
 
     
-    def handle_connect(self, pkt: protocol.chat_connect):
+    async def handle_connect(self, pkt: protocol.chat_connect):
         reply = protocol.chat_connack()
         
         if pkt.protocol_version != protocol.SERVER_CONFIG.CURRENT_VERSION:
             reply.conn_type = protocol.chat_connack.CONN_TYPE.WRONG_PROTOCOL_VERSION.value
-            self.client.send(reply.pack())
+            await self.client.send(reply.pack())
             raise protocol.WrongProtocolVersionError('[-]Wrong protocol version')
 
-        if self.clients.add_if_not_exists(pkt.username, self.client):
-            print(f"[*]{pkt.username} has connected to the server")
+        if pkt.username not in self.clients:
+            self.clients[pkt.username] = self.client
+            self.logger.info(f"[*]{pkt.username} has connected to the server")
             reply.conn_type = protocol.chat_connack.CONN_TYPE.CONN_ACCEPTED.value
             self.username = pkt.username
-            self.client.send(reply.pack())
+            await self.client.send(reply.pack())
             return
 
         reply.conn_type = protocol.chat_connack.CONN_TYPE.CONN_RETRY.value
-        self.client.send(reply.pack())
+        await self.client.send(reply.pack())
 
 
-    def handle_msg(self, pkt: protocol.chat_msg):
-        print(pkt)
+    async def handle_msg(self, pkt: protocol.chat_msg):
+        self.logger.debug(pkt)
 
-        fail = False
         if pkt.dst:
-            receiver = self.clients.get_if_exists(pkt.dst)
-            if receiver:
-                try:
-                    receiver.send(pkt.pack())
-                except:
-                    fail = True
-            else:
-                fail = True
-
-            if fail:
+            if pkt.dst not in self.clients:
                 fail_pkt = protocol.chat_msg()
                 fail_pkt.src = protocol.SERVER_CONFIG.SERVER_NAME
                 fail_pkt.dst = self.username
                 fail_pkt.msg = f"{pkt.dst} is offline"
-                self.client.send(fail_pkt.pack())
+                await self.client.send(fail_pkt.pack())
+            else:
+                receiver = self.clients[pkt.dst]
+                try:
+                    await receiver.send(pkt.pack())
+                except Exception as e:
+                    self.logger.exception(f'Error {e} inside handle_msg')
+                    pass
         else:
-            self.broadcast(pkt.pack())
+            await self.broadcast(pkt.pack())
 
 
-    def handle_command(self, pkt: protocol.chat_command):
+    async def handle_command(self, pkt: protocol.chat_command):
         match pkt.comm_type:
             case pkt.COMM_TYPE.COMM_MEMBERS.value:
                 reply = protocol.chat_msg()
                 reply.src = protocol.SERVER_CONFIG.SERVER_NAME
                 reply.dst = self.username
-                clients = self.clients.copy_keys()
-                clients.remove(self.username)
+                clients = [k for k in self.clients.keys() if k != self.username]
                 if not clients:
                     reply.msg = "You are alone in the chat"
                 else:
-                    reply.msg = '\n'.join(self.clients.copy_keys())
-                self.client.send(reply.pack())
+                    reply.msg = '\n'.join(clients)
+                await self.client.send(reply.pack())
             case _:
                 raise protocol.WrongProtocolTypeError(f"[-]Unexpected type {pkt.comm_type}")
                     
 
-    def broadcast(self, msg: bytes):
-        clients = self.clients.copy_values()
-        if self.client in clients:
-            clients.remove(self.client)
+    async def broadcast(self, msg: bytes):
+        clients = [v for v in self.clients.values() if v != self.client]
 
         for client in clients:
             try:
-                client.send(msg)
+                await client.send(msg)
             except Exception as e:
-                print(f'[-]Error while broadcasting a message to {client}: {e}')
+                self.logger.exception(f'[-]Error while broadcasting a message to {client}: {e}')
 
 
-    def shutdown(self):
-        self.client.close()
+    async def shutdown(self):
+        await self.client.close()
         if self.username:
             del self.clients[self.username] 
 
@@ -186,4 +164,4 @@ class ClientHandler:
             pkt.src = protocol.SERVER_CONFIG.SERVER_NAME
             pkt.dst = ""
             pkt.msg = f"{self.username} disconnected"
-            self.broadcast(pkt.pack())
+            await self.broadcast(pkt.pack())
